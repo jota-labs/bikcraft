a17899caa3cd731d9587d166e3599d684a8b6cc9
jota-labs <joaofilipe.wrk@gmail.com>
2026-05-19 17:39:29 -0300

    otimização

--- js/script.js
+++ js/script.js
@@ -37,22 +37,29 @@ function ativarPergunta(event) {
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener("click, ativarPergunta");
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
 
 //galeria de bicicleta
-const galeria = document.querySelectorAll(".bicicleta-imagens img")
-const galeriaContainer = document.querySelector(".bicicleta-imagens")
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-function trocarImage(event){
+function trocarImagem(event) {
   const img = event.currentTarget;
-  galeriaContainer.prepend(img)
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
 }
 
-function eventosGaleria(img){
- img.addEventListener('click', trocarImage)
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
 }
 galeria.forEach(eventosGaleria);
 
+//Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
